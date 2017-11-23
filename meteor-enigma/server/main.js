32d0f0a1da2f036d82141831ce4794d748b068df
Georgi Marinov <venom1724@gmail.com>
2017-11-24 01:25:01 +0200

    wip

--- meteor-enigma/server/main.js
+++ meteor-enigma/server/main.js
@@ -35,9 +35,9 @@ Meteor.methods({
         var command2 = "cd "+img._storagePath+" && bash all.sh "+realName;
         var command3 = "cd "+img._storagePath+" &&tesseract --psm 1 --oem 1 -l eng out_"+realName+" stdout";
 
-        console.log("Splitting image...");
+        console.log("1-Splitting image...");
         child.exec(command, Meteor.bindEnvironment( function(e,x,y){
-            console.log("Split completed!!!");
+            console.log("1-Split completed!!!");
             Meteor.setTimeout( Meteor.bindEnvironment( function() {
                 Processed.addFile(img._storagePath+"/split_edged_blurred_"+realName, { fileName:"edges", type:img.type, userId:id, meta:{}});
                 Processed.addFile(img._storagePath+"/split_contours_"+realName, { fileName:"contours" , type:img.type, userId:id, meta:{}});
@@ -45,16 +45,17 @@ Meteor.methods({
             }),700);
             
 
-            console.log("Cleaning image and doing OCR...");
+            console.log("2-Cleaning image and doing OCR...");
 
             child.exec(command2, Meteor.bindEnvironment(function(e,x,y){
-                console.log("Image cleaned");
+                console.log("2-Image cleaned");
                 Meteor.setTimeout( Meteor.bindEnvironment( function() {
                     Processed.addFile(img._storagePath+"/out_"+realName, { fileName:"final" , type:img.type, userId:id, meta:{}});
                 }),700);
 
                 child.exec(command3, Meteor.bindEnvironment(function(e,x,y){
-                    console.log("All done!");
+                    console.log("3-All done!");
+                    console.log(x);
                     OCR.insert({ text:x , userId:id});
                 }));
             }));
