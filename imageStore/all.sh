7325124bee55ae6d9df410427f449d27dfcc3329
Georgi Marinov <venom1724@gmail.com>
2017-11-24 01:06:22 +0200

    wip

--- imageStore/all.sh
+++ imageStore/all.sh
@@ -1,22 +1,22 @@
 #python ../preprocess/fft.py --image "$1" -e -bin
-python ../preprocess/split.py --image "$1"
+#python ../preprocess/split.py --image "$1"
 #python ../preprocess/fft.py --image "split_final_$1" -e -bin -v
 
 #olddpi=$(convert "split_final_$1" -format "%[resolution.x]" info:)
 
 filter=25
 offset=18
-if [ "$2" != "" ]; then
+if [ "$1" != "" ]; then
     filter=$2
 fi
 
-if [ "$3" != "" ]; then
+if [ "$2" != "" ]; then
     offset=$3
 fi
 
 
 #python ../preprocess/deskew.py --image "split_final_$1"
-textcleaner -g -e normalize -f "$filter" -o "$offset" -s 1 -a 0 -t 0 -T "split_final_$1" "out_$1"
+textcleaner -g -e normalize -f "$filter" -o "$offset" -s 1 -a 0 -t 0 -T "split_final_$1" stdout
 
 tesseract --psm 1 --oem 1 -l eng "out_$1" "out_$1"
 #-c tessedit_write_images=true
